3879bfa64553eb150731e7f409f26c4e1cc32ab7
KarlyMakowski <liropache35@gmail.com>
2022-08-23 21:07:57 +0000

    karly solved padding problem and changed navbar location

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -17,7 +17,7 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
+                    <Navbar/>
                     <Routes>
                         <Route element={<Home />} exact path="/" />
                         {/*
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Navbar } from "../component/navbar";
 import "../../styles/home.css";
 
 export const Home = () => {
 	return (
 		<div className="home">
-			<Navbar/>
+
 		</div>
 	);
 };
