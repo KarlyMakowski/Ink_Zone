d7a0c4c0fb38d4ae250d4a98d08368216a54e24f
Binkitubo <davidlolleria@hotmail.com>
2022-08-25 18:36:22 +0000

    Karly - Created tattoo's style cards

--- src/front/js/component/tattooStyles.js
+++ src/front/js/component/tattooStyles.js
@@ -1,66 +1,60 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import "../../styles/tattooStyles.css";
-import Image1 from "../../img/Neotraditional.jpeg";
-import { FaHeart } from "react-icons/fa";
+import Image1 from "../../img/realism.jpg";
+import Image2 from "../../img/Neotraditional.jpg";
+import Image3 from "../../img/japanese.jpg";
+import Image4 from "../../img/oldSchool.jpg";
 
 export const TattooStyles = () => {
   return (
     <div className="container">
-      <div className="row">
-        <div className="image">
-          <img src={Image1} alt="Neotraditional Sketch" />
-          <div className="details">
-            <h2>
-              <span>Neo-traditional</span> style
-            </h2>
-            <p>
-              Typically, a Neo-traditional tattoo features bold lines, an
-              illustrative look, highly saturated modern colors, and a feeling
-              of dimension without being exaggerated or 3D
-            </p>
-            <div className="like">
-              <Link to="/favourites">
-                <FaHeart />
-              </Link>
-            </div>
+      <div className="cards grid">
+      <div className="card">
+          <div className="img-box">
+            <img src={Image1} alt="Neotraditional Sketch" />
+          </div>
+          <div className="card-content">
+            <h1 className="card-heading">Realism</h1>
+            <p className="card-text">Realism tattoos are an art style that 
+            results in tattoos that are like a photograph or a real-life 3D object. 
+            They're great for doing portraits of people or animals, close-ups...</p>
           </div>
         </div>
-      </div>
-      <div className="image">
-        <img src={Image1} alt="Neotraditional Sketch" />
-        <div className="details">
-          <h2>
-            <span>Neo-traditional</span> style
-          </h2>
-          <p>
-            Typically, a Neo-traditional tattoo features bold lines, an
-            illustrative look, highly saturated modern colors, and a feeling of
-            dimension without being exaggerated or 3D
-          </p>
-          <div className="like">
-            <Link to="/favourites">
-              <FaHeart />
-            </Link>
+
+        <div className="card">
+          <div className="img-box">
+            <img src={Image2} alt="Neotraditional Sketch" />
+          </div>
+          <div className="card-content">
+            <h1 className="card-heading">Neo traditional</h1>
+            <p className="card-text">Its techniques involve deeper dimensions and a 
+            variety of line widths and colors. 
+            Because of modern techniques, Neo-traditional tattoos also take on a more 
+            3-dimensional appearance.</p>
           </div>
         </div>
-      </div>
-      <div className="image">
-        <img src={Image1} alt="Neotraditional Sketch" />
-        <div className="details">
-          <h2>
-            <span>Neo-traditional</span> style
-          </h2>
-          <p>
-            Typically, a Neo-traditional tattoo features bold lines, an
-            illustrative look, highly saturated modern colors, and a feeling of
-            dimension without being exaggerated or 3D
-          </p>
-          <div className="like">
-            <Link to="/favourites">
-              <FaHeart />
-            </Link>
+
+        <div className="card">
+          <div className="img-box">
+            <img src={Image3} alt="Neotraditional Sketch" />
+          </div>
+          <div className="card-content">
+            <h1 className="card-heading">Japanese</h1>
+            <p className="card-text">The process of Japanese tattooing is known as Irezumi 
+            and can be done using non-electrical tools (Tebori technique) 
+            or the mainstream tattoo needle.</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="img-box">
+            <img src={Image4} alt="Neotraditional Sketch" />
+          </div>
+          <div className="card-content">
+            <h1 className="card-heading">Old School</h1>
+            <p className="card-text">A tattoo style featuring bold black outlines 
+            and a limited color palette, with common motifs influenced by sailor tattoos.</p>
           </div>
         </div>
       </div>
--- src/front/js/views/sign-up.js
+++ src/front/js/views/sign-up.js
@@ -8,22 +8,31 @@ import { FaFacebookF, FaEye, FaEyeSlash } from "react-icons/fa";
 export const SignUp = () => {
 
     const [info, setInfo] = useState({ email: "", password: "", confirmPassword: "", name: "", });
-    const [show, setShow] = useState(false);
+    const [show, setShow] = useState(true);
+    const [show2, setShow2] = useState(true);
 
     const handleSubmit = (e) => {
         e.preventDefault();
     };
+    
     const handleChange = e => {
         setInfo({
             ...info,
             [e.target.name]: e.target.value
         })
     }
-    const toogleBtn = () => {
+
+    const toggleBtn = () => {
         setShow((prevState) => !prevState);
     };
 
+    const toggle2Btn = () => {
+        setShow2((prevState) => !prevState);
+    };
+
+
     return (
+        <div className="register">
         <div className="login-container">
             <input type="checkbox" id="toggle" />
             <div className="signin">
@@ -40,9 +49,9 @@ export const SignUp = () => {
                         <label className="floatingInput">Email</label>
                     </div>
                     <div className="form-floating d-flex">
-                        <input type={!show ? "password" : "text"} placeholder="Password" name="password" className="form-control" id="floatingPassword" autoComplete="off" value={info.password} onChange={handleChange} />
+                        <input type={!show ? "text" : "password"} placeholder="Password" name="password" className="form-control" id="floatingPassword" autoComplete="off" value={info.password} onChange={handleChange} />
                         <label className="floatingPassword mt-2">Password </label>
-                        <div className="form-control icon-eye show-password mt-2" onClick={toogleBtn}>
+                        <div className="form-control icon-eye show-password mt-2" onClick={toggleBtn}>
                             {show ? (
                                 <FaEyeSlash className="fa-2x svg" />
                             ) : (
@@ -78,9 +87,9 @@ export const SignUp = () => {
                         <label className="floatingInput">Email</label>
                     </div>
                     <div className="form-floating">
-                        <input type={!show ? "password" : "text"} placeholder="Password" name="password" className="form-control" id="floatingPassword" autoComplete="off" value={info.password} onChange={handleChange} />
+                        <input type={!show ? "text" : "password"} placeholder="Password" name="password" className="form-control" id="floatingPassword" autoComplete="off" value={info.password} onChange={handleChange} />
                         <label className="floatingPassword">Password</label>
-                        <div className="form-control icon-eye show-password2 mt-2" onClick={toogleBtn}>
+                        <div className="form-control icon-eye show-password2 mt-2" onClick={toggleBtn}>
                             {show ? (
                                 <FaEyeSlash className="fa-2x svg" />
                             ) : (
@@ -89,10 +98,10 @@ export const SignUp = () => {
                         </div>
                     </div>
                     <div className="form-floating">
-                        <input type={!show ? "password" : "text"} placeholder="Confirm Password" name="confirmPassword" className="form-control" id="floatingPassword" autoComplete="off" value={info.confirmPassword} onChange={handleChange} />
+                        <input type={!show2 ? "text" : "password"} placeholder="Confirm Password" name="confirmPassword" className="form-control" id="floatingPassword" autoComplete="off" value={info.confirmPassword} onChange={handleChange} />
                         <label className="floatingPassword">Confirm Password</label>
-                        <div className="form-control icon-eye show-password2 mt-2" onClick={toogleBtn}>
-                            {show ? (
+                        <div className="form-control icon-eye show-password2 mt-2" onClick={toggle2Btn}>
+                            {show2 ? (
                                 <FaEyeSlash className="fa-2x svg" />
                             ) : (
                                 <FaEye className="fa-2x svg" />
@@ -103,6 +112,7 @@ export const SignUp = () => {
                     <small>Already have an account? <label for="toggle">Sign In</label></small>
                 </form>
             </div>
+        </div>
         </div>
     );
 };
