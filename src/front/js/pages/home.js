9e2196a438c77b2734b041e9b7d08635f2a5b0a9
KarlyMakowski <liropache35@gmail.com>
2022-08-31 15:00:02 +0000

    made component responsive

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,7 +3,6 @@ import React from "react";
 import "../../styles/home.css";
 
 import { Carousel } from "../component/carousel";
-import { Searchbar } from "../component/searchbar";
 import { TattooStyles } from "../component/tattooStyles";
 
 export const Home = () => {
@@ -16,7 +15,6 @@ export const Home = () => {
         <div className="home-tattooStyles">
           <div className="styles-components">
             <h2 className="styles-title">Styles</h2>
-            <Searchbar />
           </div>
           <div className="grid">
             <TattooStyles />
