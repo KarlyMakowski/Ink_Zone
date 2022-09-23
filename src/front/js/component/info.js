f68a0dcb10907ee89fb540bc2b6dafb810968978
KarlyMakowski <liropache35@gmail.com>
2022-09-23 23:44:39 +0000

    removed p tag from styles card. Button added instead. Fixed media queries. Changed fav button into info button

---
+++ src/front/js/component/info.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../../styles/info.css";
+
+export const Info = () => {
+
+    return (
+        <button className="add-fav">
+            <span aria-label="Fav Style" role="img">Learn More</span>
+        </button>
+    );
+}
--- src/front/js/component/tattooStyles.js
+++ src/front/js/component/tattooStyles.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import { Favs } from "../component/favs";
+import { Info } from "../component/info";
 
 import "../../styles/tattooStyles.css";
 
@@ -10,16 +10,13 @@ export const TattooStyles = () => {
   return store.styles.map((singleStyle, i) => {
     return (
       <div className="styles-card-container" key={i}>
-        <div className="fav-btn">
-          <Favs />
-        </div>
         <div className="styles-card">
           <div className="styles-img-box">
             <img src={singleStyle.image} alt="Tattoo Style" />
           </div>
           <div className="styles-card-content">
             <h1 className="styles-card-heading">{singleStyle.style}</h1>
-            <p className="styles-card-text">{singleStyle.information}</p>
+              <Info />
           </div>
         </div>
       </div>
